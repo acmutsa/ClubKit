3755e1df5678ac6fcd018540ff0f5b12fa7cffd3
Christian Walker <christiandwalker2021@gmail.com>
2024-05-18 15:57:23 -0500

    Add Detail links

--- apps/web/src/components/events/EventCardComponent.tsx
+++ apps/web/src/components/events/EventCardComponent.tsx
@@ -9,7 +9,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
-import { Separator } from "../ui/separator";
+import defaultImg from "../../../public/img/test_image.webp";
 
 
 
@@ -18,7 +18,7 @@ export default function EventCardComponent({ event }: { event: EventType }) {
 
 
   return (
-    <Card className="flex flex-col items-center w-full">
+    <Card className="flex flex-col w-full">
       <CardHeader className="w-full flex flex-col justify-center items-center space-y-5">
         <CardTitle className="text-center">{event.name}</CardTitle>
         <div className="flex flex-row space-x-4">
@@ -34,17 +34,12 @@ export default function EventCardComponent({ event }: { event: EventType }) {
             );
           })}
         </div>
+        <Image src={defaultImg} alt="Event Image" />
       </CardHeader>
       <CardContent className="flex flex-col w-full space-y-5">
         {/* This will eventually change to the source passed in */}
         {/* Also look into aspect ratio stuff */}
-        <Image
-          src="/img/logos/acm.svg"
-          width={0}
-          height={0}
-          alt="Event Image"
-          className="h-auto w-28 self-center"
-        />
+
         <div className="w-full flex justify-start text-gray-600">
           <p>
             {event.start.toLocaleString("en-GB", {
@@ -58,7 +53,7 @@ export default function EventCardComponent({ event }: { event: EventType }) {
         <Link
           href={`/events/${event.id}`}
           className="w-1/2 h-full flex flex-row items-center justify-center border-r border-gray-400">
-          <h1 className=" text-primary">Details</h1>
+          <h1 className="text-primary">Details</h1>
         </Link>
 
         <Link
