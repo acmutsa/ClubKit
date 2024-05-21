d78f6cc3fd15ed4f9e9852bcd9d8832983958da5
joshuasilva414 <joshuasilva414@gmail.com>
2024-05-21 14:52:40 -0500

    Add: event form page and component layout

---
+++ apps/web/src/components/dash/admin/events/NewEventForm.tsx
@@ -0,0 +1,7 @@
+type NewEventFormProps = {
+  defaultDate: Date;
+};
+
+export default function NewEventForm({ defaultDate }: NewEventFormProps) {
+  return <div className="text-foreground">New Event Form</div>;
+}
