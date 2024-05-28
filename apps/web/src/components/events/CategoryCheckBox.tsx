f7d12723dd041705337a4a7fa62a6bca1cc66685
Christian Walker <christiandwalker2021@gmail.com>
2024-05-27 21:50:08 -0500

    Delete: styling from checkbox

--- apps/web/src/components/events/CategoryCheckBox.tsx
+++ apps/web/src/components/events/CategoryCheckBox.tsx
@@ -32,7 +32,8 @@ export default function CategoryCheckBox(props: EventsOptionsBarProps){
         <label
           htmlFor={name}
           className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-           style={{color:color}}>
+          //  style={{color:color}}
+           >
          {name}
         </label>
       </div>
