057f8015f55b6ca1f531323bed728f8fa0b7336b
Christian Walker <76548772+christianhelp@users.noreply.github.com>
2024-06-15 19:44:00 -0500

    Delete apps/web/src/components/shared/Navbar.tsx
    Remove: dupe navbar (lower vs upper)

--- apps/web/src/components/shared/Navbar.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Navbar() {
-    return (
-        <div className="flex w-full flex-row items-center justify-center no-scrollbar bg-gray-300 py-5">
-            <h1 className="text-3xl">This is just a dummy navbar</h1>
-          </div>
-    );
-}
