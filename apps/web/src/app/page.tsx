996a49d007e6f629dbe3daebb84b41c2f1c5c44e
Christian Walker <christiandwalker2021@gmail.com>
2024-06-15 21:19:01 -0400

    Update: mobile landing

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Navbar from "@/components/shared/navbar";
 export default function Home() {
 	return (
-		<div className="h-screen w-screen flex flex-col">
+		// bg-[var(--my-var,var(--my-background,pink))]
+		<div className=" h-[100dvh] flex w-screen flex-col">
 			<header>
 				<Navbar showBorder />
 			</header>
-			<main className="flex flex-1 w-full items-center justify-center">
+			<main className="flex w-full flex-1 items-center justify-center">
 				<h1 className="text-4xl font-black">ClubKit</h1>
 			</main>
 		</div>
