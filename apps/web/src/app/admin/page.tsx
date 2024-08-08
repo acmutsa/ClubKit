05556bef5c57bdb18d8558c50350e2b3bb1c8357
joshuasilva414 <joshuasilva414@gmail.com>
2024-08-07 19:12:35 -0500

    Add: shadcn charts

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-	return <div className="px-5 text-foreground">Admin Dash</div>;
+	return (
+		<div className="mx-auto max-w-6xl pt-4 text-foreground">
+			<div className="mb-5 grid grid-cols-2 px-5">
+				<h1 className="font-foreground text-3xl font-bold tracking-tight">
+					Overview
+				</h1>
+			</div>
+		</div>
+	);
 }
