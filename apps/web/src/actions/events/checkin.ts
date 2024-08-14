48f691ccd03122f28afcffdabdd421f4f7ca06fa
Christian Walker <christiandwalker2021@gmail.com>
2024-08-14 19:37:29 -0400

    Add little test

--- apps/web/src/actions/events/checkin.ts
+++ apps/web/src/actions/events/checkin.ts
@@ -4,14 +4,14 @@ import { authenticatedAction } from "@/lib/safe-action";
 import { userCheckInSchemaFormified } from "@/validators/userCheckin";
 import { checkInUser } from "@/lib/queries";
 import { UNIQUE_KEY_CONSTRAINT_VIOLATION_CODE } from "@/lib/constants/shared";
-
+// Fix this so that it will be an on-conflict instead of a try-catch
 export const checkInUserAction = authenticatedAction(
 	userCheckInSchemaFormified,
 	async ({ feedback, rating, userId, eventId }) => {
 		try {
 			await checkInUser(eventId, userId, feedback, rating);
 		} catch (e) {
-            ///@ts-expect-error could not find the type of the error and the status code is the next most accurate way of telling an issue
+      ///@ts-expect-error could not find the type of the error and the status code is the next most accurate way of telling an issue
 			if (e.code === UNIQUE_KEY_CONSTRAINT_VIOLATION_CODE) {
 				return {
 					success: false,
