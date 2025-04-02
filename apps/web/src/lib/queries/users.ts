e070e8347cb18872ec958e977ff8a584c0332956
Christian Walker <christiandwalker2021@gmail.com>
2025-04-02 16:30:07 -0500

    formatter

--- apps/web/src/lib/queries/users.ts
+++ apps/web/src/lib/queries/users.ts
@@ -1,11 +1,6 @@
 import c from "config";
 import { count, db, eq, gte, desc, sum } from "db";
-import {
-	checkins,
-	data,
-	events,
-	users,
-} from "db/schema";
+import { checkins, data, events, users } from "db/schema";
 import { getCurrentSemester } from "./semesters";
 
 export const getAdminUser = async (clerkId: string) => {
