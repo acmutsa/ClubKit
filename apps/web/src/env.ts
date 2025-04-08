82740516168eb89fe4502d83b3bbaef617822c4f
Christian Walker <christiandwalker2021@gmail.com>
2025-04-07 23:46:31 -0500

    updates env

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -3,10 +3,11 @@ import { z } from "zod";
 
 export const env = createEnv({
 	server: {
-		CLERK_SECRET_KEY: z.string().min(1),
+		CLERK_SECRET_KEY: z.string(),
 	},
 	client: {
-		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
+		NEXT_PUBLIC_BASE_URL:z.string(),
 	},
 	// If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
 	//   runtimeEnv: {
@@ -18,5 +19,6 @@ export const env = createEnv({
 	experimental__runtimeEnv: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+		NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
 	},
 });
