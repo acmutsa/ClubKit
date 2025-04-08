33ae7148f7cd199cc482ee18f9268b12e9aa3a48
Christian Walker <christiandwalker2021@gmail.com>
2025-04-07 23:46:51 -0500

    adds email functionality

---
+++ apps/web/src/lib/server/email.ts
@@ -0,0 +1,38 @@
+import { render } from "@react-email/components";
+import { ReactElement } from "react";
+import {emailsConfig} from "config";
+
+export interface SendEmailProps {
+	to: string | Array<string>;
+	subject: string;
+	body: string | ReactElement;
+	subscribed?: boolean;
+	name?: string;
+	from?: string;
+	reply?: string;
+	headers?: Record<string, string>;
+}
+
+export async function sendEmail(sendEmailProps: SendEmailProps) {
+	if (!emailsConfig.useEmailService) {
+		return;
+	}
+	sendEmailProps.body =
+		typeof sendEmailProps.body === "string"
+			? sendEmailProps.body
+			: await render(sendEmailProps.body);
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${process.env.PLUNK_API_KEY}`,
+		},
+		body: JSON.stringify(sendEmailProps),
+	};
+
+	const res = await fetch(`${process.env.PLUNK_BASE_URL}/send`, options);
+	if (!res.ok) {
+		console.error(res);
+		throw new Error("Failed to send email");
+	}
+}
