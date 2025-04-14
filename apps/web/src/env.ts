6ab4c527702bcfaa16d215cec85a46b0c6915766
Christian Walker <christiandwalker2021@gmail.com>
2025-04-14 11:42:04 -0500

    updates env

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,5 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
+import {emailsConfig} from "config";
 
 export const env = createEnv({
 	server: {
@@ -7,18 +8,12 @@ export const env = createEnv({
 	},
 	client: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-		NEXT_PUBLIC_BASE_URL: z.string(),
+		NEXT_PUBLIC_BASE_URL: (emailsConfig.useEmailService) ? z.string() : z.string().optional(),
 	},
-	// If you're using Next.js < 13.4.4, you'll need to specify the runtimeEnv manually
-	//   runtimeEnv: {
-	//     DATABASE_URL: process.env.DATABASE_URL,
-	//     OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
-	//     NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
-	//   },
-	//   For Next.js >= 13.4.4, you only need to destructure client variables:
 	experimental__runtimeEnv: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 		NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
 	},
+	emptyStringAsUndefined:true
 });
