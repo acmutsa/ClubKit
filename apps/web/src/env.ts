bfca91c9ff9b0744d7c6fbd22dda0d3254ad2512
Christian Walker <christiandwalker2021@gmail.com>
2025-04-14 11:44:03 -0500

    prettier

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,6 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-import {emailsConfig} from "config";
+import { emailsConfig } from "config";
 
 export const env = createEnv({
 	server: {
@@ -8,12 +8,14 @@ export const env = createEnv({
 	},
 	client: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
-		NEXT_PUBLIC_BASE_URL: (emailsConfig.useEmailService) ? z.string() : z.string().optional(),
+		NEXT_PUBLIC_BASE_URL: emailsConfig.useEmailService
+			? z.string()
+			: z.string().optional(),
 	},
 	experimental__runtimeEnv: {
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 		NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
 	},
-	emptyStringAsUndefined:true
+	emptyStringAsUndefined: true,
 });
