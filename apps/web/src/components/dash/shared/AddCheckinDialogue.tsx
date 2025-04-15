867b802b20130df00250655780fa0c4b476bb4bf
Carter LaVigne <carterlavigne3@gmail.com>
2025-04-15 14:48:17 -0500

    prittier check

--- apps/web/src/components/dash/shared/AddCheckinDialogue.tsx
+++ apps/web/src/components/dash/shared/AddCheckinDialogue.tsx
@@ -37,7 +37,7 @@ import z from "zod";
 import { useRouter } from "next/navigation";
 
 type Props = {
-	eventList: { id: string; name: string; start: Date}[];
+	eventList: { id: string; name: string; start: Date }[];
 	setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 	default?: {
 		eventID?: string;
@@ -147,12 +147,13 @@ function AddCheckinDialogue({ eventList, ...props }: Props) {
 														value={event.id}
 														key={event.id}
 														className="w-full justify-between"
-														>
-															<div className="w-full flex mx-0 gap-x-1">
-																<p>{event.name}</p>
-																<p className="opacity-60">{event.start.toLocaleDateString()}</p>
-															</div>
-																
+													>
+														<div className="mx-0 flex w-full gap-x-1">
+															<p>{event.name}</p>
+															<p className="opacity-60">
+																{event.start.toLocaleDateString()}
+															</p>
+														</div>
 													</SelectItem>
 												))}
 											</SelectContent>
